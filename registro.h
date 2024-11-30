929d4465c77f6b16e1adc263717612258cefc525
Jose <jvilcanquic@unjbg.edu.pe>
2024-11-29 20:11:24 -0500

    Añadiendo archivo de cabecera

---
+++ registro.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 10;
+struct tRegistro{
+	int codigo;
+	char item[MAX];
+	int valor;
+};
+int SIZE = sizeof(tRegistro);
+
+void mostrar(tRegistro &reg){
+	cout << "Codigo: " << reg.codigo << endl;
+	cout << "Item: " << reg.item << endl;
+	cout << "Valor: " << reg.valor << endl;
+	
+}
